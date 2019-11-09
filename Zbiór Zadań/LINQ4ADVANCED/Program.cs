2e9ea481c9934985eabde18522127147df406bbf
Kajmank2 <49954039+Kajmank2@users.noreply.github.com>
2019-11-09 16:05:48 +0100

    Linq 3Advanceed

--- Zbiór Zadań/LINQ4ADVANCED/Program.cs
+++ Zbiór Zadań/LINQ4ADVANCED/Program.cs
@@ -8,33 +8,60 @@ namespace LINQ4ADVANCED
     {
         static void Main(string[] args)
         {
-            int i, n;
-            var list = new List<char>();
-            Console.WriteLine("Input the number of Char");
-            n = Convert.ToInt32(Console.ReadLine());
-            for (i = 0; i < n; i++)
-            {
-                Console.WriteLine("Char 1 [{0}] :", i);
-                char tempList = Convert.ToChar(Console.ReadLine());
-                list.Add(tempList);
-            }
-            var _result1 = from y in list select y;
-            Console.WriteLine(" Podaj Char do usuniecia");
-            char dChar = Convert.ToChar(Console.ReadLine());
-            char newstr = list.FirstOrDefault(x => x == dChar);
-            list.Remove(newstr);
-            Console.WriteLine("Input range to delete !  nr = ");
-            int rn1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Input range to delete !  nr = ");
-            int rn2 = Convert.ToInt32(Console.ReadLine());
-            list.RemoveRange(rn1,rn2);
-            Console.WriteLine("Podaj przedział liczb do usuniecia");
-            
+            #region Zadnako z usuwaniami Charów i Range 
+            //int i, n;
+            //var list = new List<char>();
+            //Console.WriteLine("Input the number of Char");
+            //n = Convert.ToInt32(Console.ReadLine());
+            //for (i = 0; i < n; i++)
+            //{
+            //    Console.WriteLine("Char 1 [{0}] :", i);
+            //    char tempList = Convert.ToChar(Console.ReadLine());
+            //    list.Add(tempList);
+            //}
+            //var _result1 = from y in list select y;
+            //Console.WriteLine(" Podaj Char do usuniecia");
+            //char dChar = Convert.ToChar(Console.ReadLine());
+            //char newstr = list.FirstOrDefault(x => x == dChar);
+            //list.Remove(newstr);
+            //Console.WriteLine("Input range to delete !  nr = ");
+            //int rn1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Input range to delete !  nr = ");
+            //int rn2 = Convert.ToInt32(Console.ReadLine());
+            //list.RemoveRange(rn1,rn2);
+            //Console.WriteLine("Podaj przedział liczb do usuniecia");
 
-            var _result = from z in list select z;
-            foreach (var item in _result)
+
+            //var _result = from z in list select z;
+            //foreach (var item in _result)
+            //{
+            //    Console.WriteLine("Char  : {0}" , item);
+            //}
+            #endregion
+            int n, m;
+            string[] arr;
+            Console.WriteLine("Wprowadz rozmiar tablicy");
+            n = Convert.ToInt32(Console.ReadLine());
+            arr = new string[n];
+            Console.WriteLine("Wprowadz rozmiar tablicy");
+            for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("Char  : {0}" , item);
+                Console.WriteLine("Wprowadz członków do tabliczki {0}",i);
+                arr[i] = Console.ReadLine();
+            }
+            var DisplaySTR = from x in arr select x;
+            foreach (var item in DisplaySTR)
+            {
+                Console.WriteLine(item);
+            }
+            var ns = from z in arr where z.Length >= 5 // tą piątke dodatkowo można sobie zmienna zastąpic aby wybrana była mozliwosc wpisywania
+                     orderby z descending
+                     select z;
+            IEnumerable<string> zizu = arr.Where(x => x.Length >= 5); // 2 sposob
+            foreach (var item in zizu)
+            {
+                Console.WriteLine(" Pierwsza liczba powyzej 5 znaków");
+                Console.WriteLine(item);
             }
             Console.ReadKey();
         }
---
+++ Zbiór Zadań/LINQ5AdvancedCLASS/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ5AdvancedCLASS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[] set1 = { 'X', 'Y', 'Z' };
+            int[] set2 = { 1, 2, 3 };
+            string[] set3 = { "Green", "Pink" };
+            var cartsianProduct = from letter in set1
+                                  from number in set2
+                                  from colur in set3
+                                  select new { letter, number, colur };
+            foreach (var item in cartsianProduct)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("=======================================================================");
+
+            Console.Write("\nLINQ : Generate a Cartesian Product of three sets: ");
+            Console.Write("\n---------------------------------------------------\n");
+
+            var set12CartesianProduct = set1.SelectMany( // select many wybiera wiele ale łaczy tylko 2 tabellki
+                letterList => set2.Select(numList =>
+                new { letterList, numList }));
+
+            var cartesianProduct = set12CartesianProduct.SelectMany(
+                (set1and2) => set3.Select(colorList =>
+                new { set1and2.letterList, set1and2.numList, colorList }));
+
+            Console.Write("The Cartesian Product are : \n");
+            foreach (var ProductList in cartesianProduct)
+            {
+                Console.WriteLine(ProductList);
+            }
+            Console.ReadKey();
+        }
+    }
+}
